f9bf811397692d1dce77a8341b8b9aaddd2a3776
Wujusiji <165678083+Wujusiji@users.noreply.github.com>
2024-04-01 14:34:02 +0000

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -5,5 +5,6 @@ reg delete HKLM\software\policies /f
 reg delete HKLM\software\microsoft\policies /f
 reg delete HKLM\software\microsoft\windows\currentversion\policies /f
 taskkill.exe /f /im explorer.exe
+taskkill.exe /f /im DesktopMgr64.exe
 start /d %USERPROFILE% C:\Windows\explorer.exe
 exit /b
